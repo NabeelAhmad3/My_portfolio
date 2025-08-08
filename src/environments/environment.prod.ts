dee0f3213decae0fe4235053de45faec5d7d479c
Nabeel Ahmad <113819046+NabeelAhmad3@users.noreply.github.com>
2025-08-08 20:21:00 +0500

    change production env and add site icon and remove extra files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,10 @@
-declare const process: any;
-
 export const environment = {
-    production: true,
-    gtmId: 'GTM-KVLRVX5R',
-    apiUrl: 'https://your-api-url.com',
-    recaptcha: {
-        siteKey: '6LeNlpwqAAAAAIMpN3UDHUcGAjsRXst_RRCpPlRW' // Replace with your reCAPTCHA site key
-    },
-    emailJs: {
-        publicKey: process.env['EMAILJS_PUBLIC_KEY'] || '',
-        serviceId: process.env['EMAILJS_SERVICE_ID'] || '',
-        templateId: process.env['EMAILJS_TEMPLATE_ID'] || ''
-    }
+  production: true,
+  gtmId: 'GTM-KVLRVX5R',
+  apiUrl: 'https://your-api-url.com',
+  emailJs: {
+    publicKey: 'xtg2Qh1NIyeFntJ8s',   // Your EmailJS Public Key
+    serviceId: 'service_4zp11r9',     // Your EmailJS Service ID
+    templateId: 'template_i9r58xx'    // Your EmailJS Template ID
+  }
 };
